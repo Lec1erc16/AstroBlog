cd5ec931e00d13217a7f35c14f57b27fd8f7d04d
Leclerc Mo <1764496954@qq.com>
2026-06-05 22:28:06 +0800

    初始化了博客；

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'LeclercMo的博客';
+export const SITE_DESCRIPTION = '分享骑行、技术的个人博客。';
---
+++ src/content/blog/test.md
@@ -0,0 +1,7 @@
+---
+title: 'test'
+description: '这是博客上线前的验证测试文章。'
+pubDate: '2026-06-05'
+---
+# test
+this is test.
